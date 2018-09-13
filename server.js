6147850282b8ab4568934812f78a99f178546fcd
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-09-13 13:44:06 +0800

    section 5 finish

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-let app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -16,36 +31,32 @@ hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
 
-app.use((req, res, next) => {
-    let now = new Date().toString();
-    let log = `${now}: ${req.method} ${req.url}`;
-
-    console.log(log);
-    fs.appendFile('server.log', log + '\n', (err) => {
-        if (err) {
-            console.log('Unable to append to server.log');
-        }
-    });
-    next();
-});
-
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
-
 app.get('/', (req, res) => {
     res.render('home.hbs', {
-        pageTitle: 'Home page!',
+        pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About page!'
+        pageTitle: 'About Page'
     });
 });
 
-app.listen(3000, () => {
-    console.log('server listening on localhost:3000');
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
